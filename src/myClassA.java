b0a95b792633cc3966b87dc51ad19669e4caf127
zjwind <495129137@qq.com>
2018-10-08 10:57:32 +0800

    测试

---
+++ src/myClassA.java
@@ -0,0 +1,16 @@
+/**
+ * @Description
+ * @Author zhangjiuwang
+ * @Date 2018/9/30 16:49
+ */
+
+/**
+ * @author zhangjiuwang
+ * @create 2018-09-30 16:49
+ * @Description
+ */
+public class myClassA {
+    public static void A(){
+        System.out.println("this is class A");
+    }
+}
